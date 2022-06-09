c97fdf2320cdaf9269edbdc7a29d71ff995a26c9
Haluktre <halukture0@gmail.com>
2022-06-09 04:58:08 +0300

    Add model class

--- dataScience.py
+++ dataScience.py
@@ -15,43 +15,57 @@ def geomean(x):
     y = y**(1/len(x))
     return y
 
-def b1_parameter(x,y):
-    y_total = 0
-    x_total = 0
-    value0 = 0
-    value1 = 0
-    for i in range(len(y)):
-        y_total = y_total + y[i]
-        x_total = x_total + x[i]
-    y_mean = y_total/len(y) 
-    x_mean = x_total/len(x)
+class model:  
+    def __init__(self):
+        self.b0 = None
+        self.b1 = None
+        self.tss_val = None
+        self.ess_val = None
+        self.rss_val = None
+        self.R_square = None
     
-    for j in range(len(x)):
-        value0 = value0 + (y[j] - y_mean)*(x[j] - x_mean)
-        value1 = value1 + (x[j] - x_mean)**2
-    b1 = value0/value1
-    return b1
-
-def b0_parameter(b1,x_mean,y_mean):
-    b0 = y_mean - b1*x_mean
-    return b0
-
-def leastsquares_predict(b0,b1,x):
-    y_predict = b0 + b1*x
-    return y_predict
-
-def tss_ess_rss_rsquare(y,y_predicts):
-    tss_val = 0
-    ess_val = 0
-    rss_val = 0
-    for i in range(len(y)):
-        print(i)
-        tss_val = tss_val + (y[i]-mean(y))**2
-        ess_val = ess_val + (y_predicts[i]-mean(y))**2
-        rss_val = rss_val + (y[i]-y_predicts[i])**2
-    R_square = ess_val/tss_val
+    def fit(self,x,y):
+        self.__x = x
+        self.__y = y
+        self.__y_predicts = []
+        self.__x_mean = mean(x)
+        self.__y_mean = mean(y)
+        self.b1_parameter()
+        self.b0_parameter()
+        self.leastsquares_predict()
+        self.tss_ess_rss_rsquare()
+        
+        
+    def __b1_parameter(self):
+        value0 = 0
+        value1 = 0   
+        
+        for j in range(len(self.x)):
+            value0 = value0 + (self.y[j] - self.y_mean)*(self.x[j] - self.x_mean)
+            value1 = value1 + (self.x[j] - self.x_mean)**2
+            
+        self.b1 = value0/value1 
     
-    return tss_val,ess_val,rss_val,R_square
+    def __b0_parameter(self):
+        self.b0 = self.y_mean - self.b1*self.x_mean
+    
+    def __leastsquares_predict(self):
+        for i in range(len(self.x)):
+            self.y_predicts.append(self.b0 + self.b1*self.x[i])
+    
+    
+    def __tss_ess_rss_rsquare(self):
+        self.tss_val = 0
+        self.ess_val = 0
+        self.rss_val = 0
+        for i in range(len(self.y)):
+            print(i)
+            self.tss_val = self.tss_val + (self.y[i]-mean(self.y))**2
+            self.ess_val = self.ess_val + (self.y_predicts[i]-mean(self.y))**2
+            self.rss_val = self.rss_val + (self.y[i]-self.y_predicts[i])**2
+            
+        self.R_square = self.ess_val/self.tss_val
+        
     
 
 
