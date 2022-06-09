342484da60ad69acf8b3eaed6d8e002ae7ba9ca6
Haluktre <halukture0@gmail.com>
2022-06-09 07:37:03 +0300

    Update dataScience.py

--- dataScience.py
+++ dataScience.py
@@ -1,6 +1,6 @@
 import mathematicalFunctions as mf
 
-class model:  
+class SimpleLinearRegression:  
     def __init__(self):
         self.b0 = None
         self.b1 = None
@@ -15,40 +15,40 @@ class model:
         self.__y_predicts = []
         self.__x_mean = mf.mean(x)
         self.__y_mean = mf.mean(y)
-        self.b1_parameter()
-        self.b0_parameter()
-        self.leastsquares_predict()
-        self.tss_ess_rss_rsquare()
+        self.__b1_parameter()
+        self.__b0_parameter()
+        self.__leastsquares_predict()
+        self.__tss_ess_rss_rsquare()
         
         
     def __b1_parameter(self):
         value0 = 0
         value1 = 0   
         
-        for j in range(len(self.x)):
-            value0 = value0 + (self.y[j] - self.y_mean)*(self.x[j] - self.x_mean)
-            value1 = value1 + (self.x[j] - self.x_mean)**2
+        for j in range(len(self.__x)):
+            value0 = value0 + (self.__y[j] - self.__y_mean)*(self.__x[j] - self.__x_mean)
+            value1 = value1 + (self.__x[j] - self.__x_mean)**2
             
         self.b1 = value0/value1 
     
     def __b0_parameter(self):
-        self.b0 = self.y_mean - self.b1*self.x_mean
+        self.b0 = self.__y_mean - self.b1*self.__x_mean
     
     def __leastsquares_predict(self):
-        for i in range(len(self.x)):
-            self.y_predicts.append(self.b0 + self.b1*self.x[i])
+        for i in range(len(self.__x)):
+            self.__y_predicts.append(self.b0 + self.b1*self.__x[i])
     
     
     def __tss_ess_rss_rsquare(self):
         self.tss_val = 0
         self.ess_val = 0
         self.rss_val = 0
-        for i in range(len(self.y)):
-            print(i)
-            self.tss_val = self.tss_val + (self.y[i]-mf.mean(self.y))**2
-            self.ess_val = self.ess_val + (self.y_predicts[i]-mf.mean(self.y))**2
-            self.rss_val = self.rss_val + (self.y[i]-self.y_predicts[i])**2
-            
+        for i in range(len(self.__y)):
+            print("Step:",i)
+            self.tss_val = self.tss_val + (self.__y[i]-self.__y_mean)**2
+            self.ess_val = self.ess_val + (self.__y_predicts[i]-self.__y_mean)**2
+            self.rss_val = self.rss_val + (self.__y[i]-self.__y_predicts[i])**2
+        print("Finished fit!") 
         self.R_square = self.ess_val/self.tss_val
         
     
